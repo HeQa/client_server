649fbc32380aa3cfa333b575e9ba4048df9d8dfa
HeQa <95696699+HeQa@users.noreply.github.com>
2023-11-26 14:26:36 +0200

    Add files via upload

---
+++ client/client/client.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <winsock2.h>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize winsock." << std::endl;
+        return 1;
+    }
+
+    SOCKET clientSocket;
+    if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        std::cerr << "Failed to create socket." << std::endl;
+        return 1;
+    }
+
+    SOCKADDR_IN serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8000);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Failed to connect to server." << std::endl;
+        closesocket(clientSocket);
+        return 1;
+    }
+
+    char buffer[4096];
+    std::string message;
+
+    while (true) {
+        std::cout << "Enter a message: ";
+        std::getline(std::cin, message);
+        strcpy(buffer, message.c_str());
+        send(clientSocket, buffer, strlen(buffer), 0);
+
+        memset(buffer, 0, sizeof(buffer));
+        int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        if (bytesRead <= 0) {
+            break;
+        }
+
+        message = buffer;
+        std::cout << "Received response from server: " << message << std::endl;
+    }
+
+    std::cout << "Server disconnected." << std::endl;
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ client/server/server.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <winsock.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize winsock" << std::endl;
+        return -1;
+    }
+
+    // Создание серверного сокета
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to create socket" << std::endl;
+        WSACleanup();
+        return -1;
+    }
+
+    // Установка параметров адреса сервера
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(8080);
+
+    // Привязка сокета к адресу сервера
+    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Failed to bind socket" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    // Ожидание подключения клиента
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Failed to listen for incoming connections" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Server started. Waiting for clients..." << std::endl;
+
+    // Принятие подключения от клиента
+    SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to accept client connection" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Client connected" << std::endl;
+
+    // Отправка данных клиенту
+    std::string message = "Hello from server";
+    if (send(clientSocket, message.c_str(), message.size(), 0) == SOCKET_ERROR) {
+        std::cerr << "Failed to send data to client" << std::endl;
+        closesocket(clientSocket);
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Data sent to client" << std::endl;
+
+    // Закрытие сокетов и освобождение ресурсов
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
